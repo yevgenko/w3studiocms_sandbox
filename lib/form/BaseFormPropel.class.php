da5eac5c6ea3aadb921d0d3e0a6f0ac70c6ab7f3
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2009-03-29 20:20:53 +0300

    add BaseFormPropel class

---
+++ lib/form/BaseFormPropel.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Project form base class.
+ *
+ * @package    w3studiocms_sandbox
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+abstract class BaseFormPropel extends sfFormPropel
+{
+  public function setup()
+  {
+  }
+}
